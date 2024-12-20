7ec5ddc9e3cf80101fda430f059d989781af276a
gatsuweb <ivan.duran@outlook.fr>
2024-12-20 20:39:19 +0100

    commit

--- app/components/SplineMe.tsx
+++ app/components/SplineMe.tsx
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/StAeqFyMFZ2yZTqx/scene.splinecode" />
+<Spline scene="https://prod.spline.design/9iXhfBRorMazRNQK/scene.splinecode" />
   );
 }
